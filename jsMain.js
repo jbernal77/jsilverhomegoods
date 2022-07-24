0cd63d2931137dceef08f05338da687a7cc09b39
PaytonMcCormick <payton.mccormick34@gmail.com>
2022-07-24 02:29:43 -0400

    initialized productTemplate

--- jsMain.js
+++ jsMain.js
@@ -1,5 +1,5 @@
 /******************************************
- *  Author : Payton McCormick
+*  Author : Payton McCormick, Jonathan Bernal, Adam Mazur, Sajen Vasuthevan
  *  Created On : Fri Jul 08 2022
  *  File : jsMain.js
  *******************************************/
---
+++ jsProduct.js
@@ -0,0 +1,5 @@
+/******************************************
+ *  Author : Payton McCormick, Jonathan Bernal, Adam Mazur, Sajen Vasuthevan
+ *  Created On : Sun Jul 24 2022
+ *  File : jsProduct.js
+ *******************************************/
