6163b2716e642370de5377e1074bcbbb90f328e6
PaytonMcCormick <payton.mccormick34@gmail.com>
2022-08-02 12:25:24 -0400

    front-end additions

--- categoryPage.php
+++ categoryPage.php
@@ -16,73 +16,91 @@
     <meta name="keywords" content=""> <!-- TODO: Fill me in-->
 
     <!--CSS-->
-    <link rel="stylesheet" type="text/css" href="/style.css"> <!-- TODO: link CSS -->
+    <link rel="stylesheet" type="text/css" href="/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!--JS-->
     <script src=""></script>
-    <!--TODO: link JS -->
+
+    <!--Bootstrap JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+            crossorigin="anonymous">
+    </script>
 
 
-    <title></title>
+    <title>Category Page</title>
     <!--TODO Change based on category -->
 </head>
 
 <body class="container-fluid">
 
-    <!--NAVBAR-->
-    <div class="row">
-        <div class="row ">
-            <div class="col-12 p-0">
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                </div>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Cart</a>
-                            </li>
-                        </ul>
-                        <form class="form-inline my-2 my-lg-0 d-flex justify-content-end">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
-
-                    </div>
-                </nav>
+    <!--NavBar-->
+    <nav class = "navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class = "container">
+            <a href = "index.html" class = "navbar-brand" id = "headingPage">JSilver Home Goods</a>
+            
+            <button 
+            class = "navbar-toggler"
+            type = "button" 
+            data-bs-toggle = "collapse" 
+            data-bs-target = "#rightMenu">
+            <span class = "navbar-toggler-icon"></span>
+            </button>            
+        
+            <div class = "collapse navbar-collapse" id = "rightMenu">
+                <ul class="nav navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Shopping Cart</a>
+                    </li>
+                </ul>
             </div>
         </div>
-    </div>
-    <!-- END OF NAVBAR-->
+    </nav>
 
-    <div class="h1 text-center m-3">Category</div>
+    <div class="h1 text-center m-3">
+        <?php
+
+        $servername = "localhost";
+        $username = "bernal_finalProjectDatabase";
+        $password = "jsilverhomegoods";
+        $dbname = "bernal_finalProjectDatabase";
+
+
+        $header = $_GET['category'];
+        $headerUpper = strtoupper($header);
+        echo $headerUpper;
+
+        $conn = new mysqli($servername,$username,$password);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT  * FROM `Products` WHERE productCategory = '$header'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row == $result->fetch_assoc()){
+                echo "Title: ". $row["productName"];
+            }
+        }
+
+        ?>
+    </div>
     <br>
 
     <!--PRODUCTS-->
     <div class="row">
-        <div class="col-12">
+        <div class="col-12 d-flex">
             <div class="card m-1" style="width: 14rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
+                <img class="card-img-top" src="\" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
