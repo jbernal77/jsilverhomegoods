ab9736038a5e4b076d8569e515ea34f87fdcfb01
PaytonMcCormick <payton.mccormick34@gmail.com>
2022-08-07 20:28:07 -0400

    sign-up integrated

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'bernal_finalProjectDatabase');
+define('DB_PASSWORD', 'jsilverhomegoods');
+define('DB_NAME', 'bernal_finalProjectDatabase');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
