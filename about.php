1e1fe99df8ad669fc2fb58e1633a5d74103c22bc
PaytonMcCormick <payton.mccormick34@gmail.com>
2022-08-08 21:58:00 -0400

    about.php content added

--- about.php
+++ about.php
@@ -6,5 +6,18 @@
 
 <?=template_header('About')?>
 
+    <div class="content-wrapper">
+        <h2>About</h2>
+        <br>
+        <p>JSilverHomegoods offers 100% handmade goods at a reasonable price. We take pride in creating our products and hope that you find each and every 
+            product to be top quality.</p>
+        <br>
+        <h4>Contact Info:</h4>
+        <p>Phone: (123) 456-7890<br>
+            Address: 123 Fake St, Windsor ON<br>
+            Email: jsilverhomegoods@gmail.com<br>
+        </p>
+        
+    </div>
 
 <?=template_footer()?>
