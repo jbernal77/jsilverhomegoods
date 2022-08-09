c3796a1d2baaf2d9a612fb09ac33ea54aaf2b5f6
PaytonMcCormick <payton.mccormick34@gmail.com>
2022-08-08 22:30:45 -0400

    faq content added

--- faq.php
+++ faq.php
@@ -7,5 +7,18 @@
 
 <?=template_header('FAQ')?>
 
-
+    <div class="content-wrapper">
+        <h4>FAQ</h4>
+        <p><b>Q</b>: Are the stickers permanent?<br>
+            A: The stickers are made with permanent vinyl which means that they should not peel off unless forced. Normal wear and tear might occur and result in corners rising, however you can use mod-podge to seal the sticker if you so choose.<br>
+            <br>
+            <b>Q</b>: How do I create a custom order?<br>
+            A: Please email jsilverhomegoods@gmail.com for any custom orders. You may need to provide images if you desire a specific design.<br>
+            <br>
+            <b>Q</b>: When will my order arrive?<br>
+            A: If you are located in Windsor/Essex county, anywhere from 1-2 days. If you are located outside of Windsor/Essex, expect 3-5 business days for shipping.<br>
+            <br>
+            <b>Q</b>: Is there a pickup option?<br>
+            A: Yes! You can specify pick-up to negate the shipping fees upon ordering. You can pick your items up at 123 Fake St, Windsor, ON.<br>
+    </div>
 <?=template_footer()?>
