41ed9e004795335dcf02fc8ca535f17840a5275a
PaytonMcCormick <payton.mccormick34@gmail.com>
2022-08-09 13:53:33 -0400

    init shipping page

---
+++ shipping.php
@@ -0,0 +1,21 @@
+<!-- ******************************************
+*  Author : Payton McCormick, Jonathan Bernal, Adam Mazur, Sajen Vasuthevan
+*  Created On : Tuesday August 9 2022
+*  File : terms.php
+******************************************* -->
+
+<?=template_header('Shipping')?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<meta name="description" content="JSilverHomegoods Shipping Information">   
+<meta name="keywords" content="JSilverHomegoods Crafts Shipping Package">
+
+<?=template_navbar('Shipping')?>
+
+<h2>Shipping Information</h2>
+
+<p>Upon placing your order, you will be given options for shipping. You can expect your package to arrive within 1-2 business days if you are located inside Windsor-Essex County. If you
+    are located outside of the area, expect your package to arrive within 3-5 business days. There is also an option to decline shipping, and pick up at our location at 123 Fake Street, Windsor ON. You will
+    not be charged for shipping and handling if you choose this option. <br>
+    There is a flat fee of $5 for any package shipped outside of Windsor/Essex. Any orders placed outside of Ontario will be subject to an additional $5 fee for long distance shipping. This does not include duty fees at the border.<br>
+    Every product that we ship is carefully packed and protected with environmentally friendly packaging. We want to ensure that all of our products arrive in one piece. For any shipping concerns, please contact us at jsilverhomegoodsshipping@gmail.com 
+</p>
