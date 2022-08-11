33d7b19bcda5e4a83be400caca58a8b11697f898
PaytonMcCormick <payton.mccormick34@gmail.com>
2022-08-10 22:17:52 -0400

    cleaned up faq

--- faq.php
+++ faq.php
@@ -12,6 +12,15 @@
 <?=template_navbar('About')?>
 
     <div class="content-wrapper">
+        <h4>How to use our website</h4>
+        <b>Q</b>: How do I create an account?<br>
+            A: To create an account click account at the top of the website, sign up now, fill in the required information and youre done.<br>
+            <br>
+			<b>Q</b>: How do I make an order?<br>
+            A: After creating an account you may begin your order. Simply select the item youd like to purchase, select quantity, add to cart, click on your cart and then place order.<br>
+            <br>
+            
+
         <h4>FAQ</h4>
         <p><b>Q</b>: Are the stickers permanent?<br>
             A: The stickers are made with permanent vinyl which means that they should not peel off unless forced. Normal wear and tear might occur and result in corners rising, however you can use mod-podge to seal the sticker if you so choose.<br>
@@ -24,13 +33,8 @@
             <br>
             <b>Q</b>: Is there a pickup option?<br>
             A: Yes! You can specify pick-up to negate the shipping fees upon ordering. You can pick your items up at 123 Fake St, Windsor, ON.<br>
-			<b>Q</b>: How do I create an account?<br>
-            A: To create an account click account at the top of the website, sign up now, fill in the required information and youre done.<br>
-            <br>
-			<b>Q</b>: How do I make an order?<br>
-            A: After creating an account you may begin your order. Simply select the item youd like to purchase, select quantity, add to cart, click on your cart and then place order.<br>
-            <br>
-			<b> If you have any other questions please email jsilverhomegoods@gmail.com.<br></b>
-            <br>
+
+            <b> If you have any other questions please email jsilverhomegoods@gmail.com.<br></b>
+			
     </div>
 <?=template_footer()?>
